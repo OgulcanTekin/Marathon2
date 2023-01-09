a10d2351083d907903873bb697435700cdc9ba44
OgulcanTekin <ogulcantekin97@gmail.com>
2023-01-09 17:23:17 +0300

    Marathon2

--- src/main/java/com/ogulcan/Main.java
+++ src/main/java/com/ogulcan/Main.java
@@ -1,7 +1,12 @@
 package com.ogulcan;
 
+import com.ogulcan.Utility.Menu;
+import com.ogulcan.Utility.Utility;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("deneme");
+        Menu.menu();
+
     }
 }
